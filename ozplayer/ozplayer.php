53b3db30f4d57e2ff05ad555e667d04405952885
Matt McLeod <matt@accessibilityoz.com.au>
2014-08-26 16:49:43 +1000

    First cut. No admin panel yet.

--- ozplayer/ozplayer.php
+++ ozplayer/ozplayer.php
@@ -6,10 +6,33 @@
  * Version: 0.1
  * Author: Matt McLeod
  * Author URI: http://accessibilityoz.com.au/
- * License: GPL
+ * License: GPL2
  */
 
+/*  Copyright 2014  AccessibilityOz  (email : matt@accessibilityoz.com.au)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 defined('ABSPATH') or die("No script kiddies please!");
+
+add_option("ozplayer_base", '/ozplayer');
+add_option("ozplayer_config_js", '/ozplayer-config.js');
+add_option("ozplayer_transcript_css",'/ozplayer/transcript.css');
+add_option("ozplayer_lang", 'en');
+add_option("ozplayer_color", 'blue');
+add_option("ozplayer_transcript_heading","Video transcript");
 
 /**
  * The Video shortcode.
@@ -34,6 +57,15 @@ defined('ABSPATH') or die("No script kiddies please!");
  *                            Default 'wp-video-shortcode'.
  *     @type string $id       The 'id' attribute for the `<video>` element.
  *                            Default 'video-{$post_id}-{$instances}'.
+ *	   @type string $color    Colour for highlights
+ *     @type string $captions URL to the captions VTT file
+ *     @type string $transcript URL to the transcript-extras VTT file
+ *     @type string $lang     Language code
+ *                            Default 'en'
+ *     @type string $mp4      URL for MP4 version of video
+ *     @type string $webm     URL for WebM version of video
+ *     @type string $mp3      URL for MP3 version of audio descriptions
+ *     @type string $ogg      URL for OGG Vorbis version of audio descriptions
  * }
  * @param string $content Optional. Shortcode content.
  * @return string HTML content to display video.
@@ -44,6 +76,10 @@ function ozp_video_shortcode( $attr, $content = '' ) {
 
 	static $instances = 0;
 	$instances++;
+
+	$ozplayer_base = get_option('ozplayer_base');
+	$config_js = get_option('ozplayer_config_js');
+	$transcript_heading = get_option('ozplayer_transcript_heading');
 
 	/**
 	 * Filter the default video shortcode output.
@@ -69,12 +105,21 @@ function ozp_video_shortcode( $attr, $content = '' ) {
 	$default_types = wp_get_video_extensions();
 	$defaults_atts = array(
 		'src'      => '',
+		'ogg'		=> '',
+		'mp3'      => '',
+		'mp4'      => '',
+		'webm'		=> '',
+		'lang'      => get_option('ozplayer_lang'),
+		'color'      => get_option('ozplayer_color'),
+		'transcript_css' => get_option('ozplayer_transcript_css'),
 		'poster'   => '',
 		'loop'     => '',
 		'autoplay' => '',
 		'preload'  => 'metadata',
 		'width'    => 640,
 		'height'   => 360,
+		'captions' => '',
+		'transcript' => ''
 	);
 
 	foreach ( $default_types as $type )
@@ -133,17 +178,17 @@ function ozp_video_shortcode( $attr, $content = '' ) {
 	}
 
 	/**
-	 * Filter the media library used for the video shortcode.
-	 *
-	 * @since 3.6.0
-	 *
-	 * @param string $library Media library used for the video shortcode.
+	 * Queue up the OzPlayer-related scripts and CSS
 	 */
-	$library = apply_filters( 'wp_video_shortcode_library', 'mediaelement' );
-	if ( 'mediaelement' === $library && did_action( 'init' ) ) {
-		wp_enqueue_style( 'wp-mediaelement' );
-		wp_enqueue_script( 'wp-mediaelement' );
-	}
+
+    wp_enqueue_script('ozp-me',$ozplayer_base . "/ozplayer-core/mediaelement.min.js",null,null,true);
+    wp_enqueue_script('ozp-ozp',$ozplayer_base . "/ozplayer-core/ozplayer.min.js",array('ozp-me'),null,true);
+    wp_enqueue_script('ozp-lang',$ozplayer_base . "/ozplayer-lang/" . $lang . ".js",array('ozp-me','ozp-ozp'),null,true);
+    wp_enqueue_script('ozp-config',$config_js,array('ozp-me','ozp-ozp','jquery'),null,true);
+
+    wp_enqueue_style('ozp-css',$ozplayer_base . "/ozplayer-core/ozplayer.min.css");
+    wp_enqueue_style('ozp-transcript',$transcript_css);
+    wp_enqueue_style('ozp-colour',$ozplayer_base . "/ozplayer-skin/highlights-" . $color . ".css","ozp-css");
 
 	/**
 	 * Filter the class attribute for the video shortcode output container.
@@ -152,15 +197,12 @@ function ozp_video_shortcode( $attr, $content = '' ) {
 	 *
 	 * @param string $class CSS class or list of space-separated classes.
 	 */
+
+	$id = sprintf( 'video-%d-%d', $post_id, $instances );
 	$atts = array(
-		'class'    => apply_filters( 'wp_video_shortcode_class', 'wp-video-shortcode' ),
-		'id'       => sprintf( 'video-%d-%d', $post_id, $instances ),
 		'width'    => absint( $width ),
 		'height'   => absint( $height ),
 		'poster'   => esc_url( $poster ),
-		'loop'     => $loop,
-		'autoplay' => $autoplay,
-		'preload'  => $preload,
 	);
 
 	// These ones should just be omitted altogether if they are blank
@@ -175,9 +217,7 @@ function ozp_video_shortcode( $attr, $content = '' ) {
 	}
 
 	$html = '';
-	if ( 'mediaelement' === $library && 1 === $instances )
-		$html .= "<!--[if lt IE 9]><script>document.createElement('video');</script><![endif]-->\n";
-	$html .= sprintf( '<video %s controls="controls">', join( ' ', $attr_strings ) );
+	$html .= sprintf( '<video %s controls="controls" preload="none">', join( ' ', $attr_strings ) );
 
 	$fileurl = '';
 	$source = '<source type="%s" src="%s" />';
@@ -203,11 +243,31 @@ function ozp_video_shortcode( $attr, $content = '' ) {
 		$html .= trim( $content );
 	}
 
-	if ( 'mediaelement' === $library )
-		$html .= wp_mediaelement_fallback( $fileurl );
-	$html .= '</video>';
+	$transcript_html = '';
+	$transcript_attr = '';
+	if (! empty($captions)) {
+		$html .= sprintf('<track src="%s" kind="captions" srclang="%s"/>', $captions, $lang);
+	}
+	if (! empty($transcript)) {
+		$html .= sprintf('<track src="%s" kind="metadata" data-kind="transcript" srclang="%s"/>', $transcript, $lang);
+		$transcript_html = sprintf('<details class="ozplayer-expander"><summary>%s</summary><div id="transcript-%s" class="ozplayer-transcript"></div></details>',$transcript_heading,$id);
+		$transcript_attr = sprintf('data-transcript="transcript-%s"',$id);
+	}
+	$html .= "</video>";
 
-	$html = sprintf( '<div style="width: %dpx; max-width: 100%%;" class="wp-video">%s</div>', $width, $html );
+	$ad_html = '';
+	if (! empty($mp3) or ! empty($ogg)) {
+		$ad_html = '<audio preload="none">';
+		if (! empty($mp3)) {
+			$ad_html .= sprintf('<source src="%s" type="audio/mp3" />',$mp3);
+		}
+		if (! empty($ogg)) {
+			$ad_html .= sprintf('<source src="%s" type="audio/ogg" />',$ogg);
+		}
+		$ad_html .= "</audio>";
+	}
+
+	$html = sprintf( '<figure class="ozplayer-container"><div id="%s" class="ozplayer" data-controls="stack" %s>%s %s</div>%s</figure>', $id, $transcript_attr, $html, $ad_html, $transcript_html );
 
 	/**
 	 * Filter the output of the video shortcode.
